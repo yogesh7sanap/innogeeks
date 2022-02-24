0aa06bf4a7dfb1d710ca79881ba68694de3490ef
yogesh <yogesh7sanap@gmail.com>
2022-02-24 16:40:56 +0530

    mongodb edutech db creation and data insertion by yogesh

---
+++ student_db_mongo.sql
@@ -0,0 +1,31 @@
+{
+    
+    "student_courses":
+    [
+        {
+        
+            "_id" : ObjectId("621731425d1652ec8e1ef6a0"),
+                "student_name" : "Rakesh",
+                "admin" : "dev_",
+                "sessions" : 42.0,
+                "assignments" : "assessmentexam"
+        },
+        
+        {
+                "_id" : ObjectId("621731425d1652ec8e1ef6a1"),
+                "student_name" : "survesh",
+                "admin" : "crack_",
+                "sessions" : 42.0,
+                "assignments" : "customerexam"
+        },
+        {
+            "_id" : ObjectId("621731425d1652ec8e1ef6a2"),
+            "student_name" : "prathmesh",
+            "admin" : "div_",
+            "sessions" : 21.0,
+            "assignments" : "clientexam"
+        }
+        
+    ]
+
+}
